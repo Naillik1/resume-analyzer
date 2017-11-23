bdf4ed85dc44aac93124cbcf64af8d01d767a78e
Killian McGuinness <kmcguin@uwo.ca>
2017-11-23 20:52:28 +0000

    correct typo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.json(data);
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`App listening on port ${port}`);
